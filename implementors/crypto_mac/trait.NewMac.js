9c0e1f08b0a176785a22c9233a4758a8bf2282ae
paritytech-ci <devops-team@parity.io>
2022-02-16 11:56:42 +0000

    Updated docs for master

--- implementors/crypto_mac/trait.NewMac.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["blake2"] = [{"text":"impl NewMac for <a class=\"struct\" href=\"blake2/struct.Blake2b.html\" title=\"struct blake2::Blake2b\">Blake2b</a>","synthetic":false,"types":["blake2::blake2b::Blake2b"]},{"text":"impl NewMac for <a class=\"struct\" href=\"blake2/struct.Blake2s.html\" title=\"struct blake2::Blake2s\">Blake2s</a>","synthetic":false,"types":["blake2::blake2s::Blake2s"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
